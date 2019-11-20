c02dcaaf30e7ce2aada8e5809686cf9211b3887d
pokomaru <37724349+pokomaru@users.noreply.github.com>
2019-11-20 16:01:13 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,5 @@ mkdir -p ~/.config/nvim
 ln -sf ~/.dotfiles/.inputrc ~/.inputrc
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/.vscode/keybindings.json ~/.config/Code/User/keybindings.json
+ln -sf ~/.dotfiles/.vscode/settings.json ~/.config/Code/User/settings.json
