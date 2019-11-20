1a25ab7c313925cbfa214cb9b7faf99da5f78ad4
pokomaru <37724349+pokomaru@users.noreply.github.com>
2019-11-20 16:08:58 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 git clone https://github.com/seebi/dircolors-solarized.git
 mkdir -p ~/.config/nvim
+mkdir -p ~/.config/Code/User/
+yum -y update && yum clean all
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install -y neovim python3-neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -sf ~/.dotfiles/.inputrc ~/.inputrc
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
